b866a3af9f721800ca604d201e6a80118600c4b4
ARVINDSHUKLA0 <shuklaarvind2521@gmail.com>
2025-06-10 16:39:04 +0530

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';  
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import './globals.css';
+"use client"; 
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import '@fortawesome/fontawesome-free/css/all.min.css';  
+import './globals.css';  
+import { useEffect } from 'react'; 
+
 export default function RootLayout({ children }) {
+  useEffect(() => {  
+    import('bootstrap/dist/js/bootstrap.bundle.min.js')
+      .then(() => {
+        console.log('Bootstrap JS loaded successfully!'); 
+      })
+      .catch(error => {
+        console.error('Error loading Bootstrap JS:', error);
+      });
+  }, []);  
+
   return (
     <html lang="en">
       <body>{children}</body>
